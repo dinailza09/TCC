363cfadd9da12ac331453ca761b3751657e90a51
dinailza09 <dinailza09@gmail.com>
2022-10-29 20:04:36 -0300

    Atualizando

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Produtos</h2>
-</body>
+  <head>
+    <!-- css -->
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+          <!-- sidebar -->
+          @include('admin.sidebar')
+          <!-- header -->
+          @include('admin.header')
+          <!-- script -->
+          @include('admin.script')
+  </body>
 </html>
