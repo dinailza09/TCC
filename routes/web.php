9627b893791ca15ec7c0fe469fa126b6b6b6fb6d
dinailza09 <dinailza09@gmail.com>
2022-10-29 16:50:12 -0300

    atualizando

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,3 +33,5 @@ Route::middleware([
 
 
 Route::get('/redirect', [HomeController::class, 'redirect']);
+
+Route::get('/view_category', [AdminController::class, 'view_category']);
