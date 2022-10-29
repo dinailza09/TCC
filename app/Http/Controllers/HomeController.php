b2492ff313d444fd02af9d408b3bf394b8cd618d
dinailza09 <dinailza09@gmail.com>
2022-10-29 12:12:02 -0300

    atualizando

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,13 @@ use App\Models\User;
 
 class HomeController extends Controller
 {
+
+    public function index(){
+        return view('home.userpage');
+    }
+
+
+
     public function redirect(){
         $usertype=Auth::user()->usertype;
 
