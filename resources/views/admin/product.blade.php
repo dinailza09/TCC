2224dc62218d23fd1a62ff84cc6ecab9e730a375
dinailza09 <dinailza09@gmail.com>
2022-10-29 21:32:42 -0300

    atualizando

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -32,7 +32,13 @@
 
           <div class="main-panel">
           <div class="content-wrapper">
-          <div class="div_center">
+          @if(session()->has('message'))
+
+<div class="alert alert-success">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+  {{session()->get('message')}}
+    </div>
+@endif
          
 <div class="div_design">
 <h2 class="font_size">Adicionar Produto<h2>
