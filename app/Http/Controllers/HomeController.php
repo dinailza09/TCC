a80f7dc411c7f4b1a48f7279a831d4e852dc8c73
dinailza09 <dinailza09@gmail.com>
2022-10-31 13:32:02 -0300

    atualizando

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -90,7 +90,12 @@ class HomeController extends Controller
             return redirect('login');
         }
 
-    }
 
+    }
+       
+    
+          public function show_cart(){
+            return view('home.showcart');
+        }
 
 }
--- resources/views/home/product_details.blade.php
+++ resources/views/home/product_details.blade.php
@@ -65,7 +65,21 @@
                         <h6>Descrição do Produto:{{$product->description}}</h6>
                         <h6>Quantidade:{{$product->quantity}}</h6>
 
-                        <a href="" class="btn btn-primary"> Add to Cart</a>
+
+                        <form action="{{url('add_cart', $product->id)}}" method="Post">
+
+                        @csrf
+
+                        <div class="row">
+                           <div class="col-md-4">  
+                           <input type="number" name="quantity" value="1" min="1" style="width:100px">
+                           </div>
+                           <div class="col-md-4"> 
+                           <input type="submit" value="Adicionar no carrinho">
+                           </div>
+
+                        </div>
+                        </form>
 
                       
                      </div>
