3ba22015fcf0cab79f23442441554eaf10ece1b1
dinailza09 <dinailza09@gmail.com>
2022-11-20 19:26:19 -0300

    atualizando

--- resources/views/admin/email_info.blade.php
+++ resources/views/admin/email_info.blade.php
@@ -16,6 +16,44 @@
           <div class="main-panel">
           <div class="content-wrapper">
 
+          <h1 style="text-align: center; font-size:23px;" >Enviar email para {{$order->email}} </h1>
+
+<form>
+          <div style="padding-left: 35%; padding-top:30px;">
+            <label>Saudação por Email:</label>
+            <input type="text" name="greeting">
+</div>
+
+<div style="padding-left: 35%; padding-top:30px;">
+            <label> Email FirstLing:</label>
+            <input type="text" name="firstline">
+</div>
+
+<div style="padding-left: 35%; padding-top:30px;">
+            <label>Corpo do Email:</label>
+            <input type="text" name="body">
+</div>
+
+<div style="padding-left: 35%; padding-top:30px;">
+            <label>Email Button name:</label>
+            <input type="text" name="button">
+</div>
+
+<div style="padding-left: 35%; padding-top:30px;">
+            <label> Email URL:</label>
+            <input type="text" name="url">
+</div>
+
+<div style="padding-left: 35%; padding-top:30px;">
+            <label> Email Last Line:</label>
+            <input type="text" name="lastline">
+</div>
+
+<div style="padding-left: 35%; padding-top:30px;">
+            <input type="submit" value="Enviar Email" class="btn btn-primary">
+</div>
+
+</form>
 </div>
 </div>
           <!-- script -->
